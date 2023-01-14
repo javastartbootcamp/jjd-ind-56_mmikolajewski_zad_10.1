3ec9aa9a2f1d2cef70525b19a137575ce3222a86
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-01-14 15:25:35 +0000

    Rozwiązanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,58 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Person[] people = new Person[3];
+    private static int peopleNumber = 0;
 
     public static void main(String[] args) {
 
+        System.out.println("Podaj informacje o " + people.length + " osobach.");
+
+        do {
+            System.out.println("Osoba numer " + (peopleNumber + 1));
+            Person createdPerson = createPerson();
+            if (!isDuplicate(createdPerson)) {
+                addPersonToTab(createdPerson);
+            } else {
+                System.out.println("Duplikat");
+            }
+        } while (peopleNumber < people.length);
+        System.out.println("Koniec Programu");
+
+        printArrayInfo();
+
+    }
+
+    private static Person createPerson() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj Imię i Nazwisko.");
+        String fullName = scanner.nextLine();
+        System.out.println("Podaj wiek.");
+        int age = scanner.nextInt();
+        return new Person(fullName, age);
+    }
+
+    private static boolean isDuplicate(Person createdPerson) {
+        for (Person person : people) {
+            if (person != null) {
+                if (createdPerson.equals(person)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void addPersonToTab(Person createdPerson) {
+        people[peopleNumber] = createdPerson;
+        peopleNumber++;
+    }
+
+    private static void printArrayInfo() {
+        for (Person person : people) {
+            System.out.println(person);
+        }
     }
 }
---
+++ src/main/java/pl/javastart/task/Person.java
@@ -0,0 +1,54 @@
+package pl.javastart.task;
+
+import java.util.Objects;
+
+public class Person {
+    String fullName;
+    int age;
+
+    public Person(String fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(fullName, person.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "fullName='" + fullName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
